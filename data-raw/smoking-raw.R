45d98272bbcfc77b7edac12b722d4a76603dbea1
Einar Holsbø <einar@cs.uit.no>
2016-09-25 18:16:02 +0200

    build smoking data into gset

--- data-raw/smoking-raw.R
+++ data-raw/smoking-raw.R
@@ -1,3 +1,5 @@
+library(geneset)
+
 former_v_never <- c("LRRN3", "RASSF1", "METRNL", "TTC38", "MAL", "GNLY",
   "TSPAN7", "PILRA", "FRAT1", "TMEM184B", "CCL5", "C5AR2", "PTGDR", "MATK",
   "ADAM23", "STAT3", "LAT2", "PRDM9", "LEF1", "CLDND1", "REXO2", "B3GNT3",
@@ -8,7 +10,11 @@ current_v_never <- c("LRRN3", "SASH1", "CLDND1", "PID1", "CLEC10A", "MAL",
   "MTSS1", "SAMD3", "LTB", "PASK", "FPR3", "FUCA1", "RHOC", "KLRF1", "OSBPL5",
   "FBXL16", "ABCB1")
 
-smoking <- list("Top 25, current smoker vs never smoker"=current_v_never,
-                "Top 25, former smoker vs never smoker"=former_v_never)
+gsnames <- c("Top 25, current smoker vs never smoker",
+           "Top 25, former smoker vs never smoker")
+gsdescriptions <- rep("http://dx.doi.org/10.1093/hmg/ddw288", 2)
+gslist <- list(current_v_never, former_v_never)
+
+smoking <- gset(gsnames, gsdescriptions, gslist)
 
 devtools::use_data(smoking, overwrite=T)
